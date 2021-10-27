86d9597a48cdd0c51a36a951e46007cb39c78733
aulusuf <[aulusuf@gmail.com]>
2021-10-27 23:35:28 +0700

    learn the basic

---
+++ pages/detail/about.js
@@ -0,0 +1,13 @@
+import React, { useState, useEffect } from "react";
+import Head from "next/head";
+
+export default function About() {
+  return (
+    <div>
+      <Head>
+        <tittle>About Me</tittle>
+      </Head>
+      <p>Haloooooo</p>
+    </div>
+  );
+}
